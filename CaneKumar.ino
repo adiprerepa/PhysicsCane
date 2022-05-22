bad537d95beaed25f116fbebc572aeea93b4aa97
Aditya Prerepa <adiprerepa@gmail.com>
2022-05-21 18:22:07 -0700

    finish

--- CaneKumar.ino
+++ CaneKumar.ino
@@ -1,6 +1,194 @@
 #define PI 3.1415926535897932384626433832795
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
+//Mario main theme melody
+int melody[] = {
+  NOTE_E7, NOTE_E7, 0, NOTE_E7,
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0,
+
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0,
+
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0
+};
+//Mario main them tempo
+int tempo[] = {
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+};
+//Underworld melody
+int underworld_melody[] = {
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, 0,
+  0, NOTE_DS4, NOTE_CS4, NOTE_D4,
+  NOTE_CS4, NOTE_DS4,
+  NOTE_DS4, NOTE_GS3,
+  NOTE_G3, NOTE_CS4,
+  NOTE_C4, NOTE_FS4, NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,
+  NOTE_GS4, NOTE_DS4, NOTE_B3,
+  NOTE_AS3, NOTE_A3, NOTE_GS3,
+  0, 0, 0
+};
+//Underwolrd tempo
+int underworld_tempo[] = {
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  6, 18, 18, 18,
+  6, 6,
+  6, 6,
+  6, 6,
+  18, 18, 18, 18, 18, 18,
+  10, 10, 10,
+  10, 10, 10,
+  3, 3, 3
+};
 int m1 = 2;
-int m2 m = 3;
+int m2  = 3;
 int echo1 = 4;
 int trig1 = 5;
 int echo2 = 7;
@@ -8,6 +196,7 @@ int trig2 = 6;
 int echo3 = 8;
 int trig3 = 9;
 int ena = 10;
+int melodyPin = 11;
 
 void setup() {
   pinMode(m1, OUTPUT);
@@ -19,9 +208,10 @@ void setup() {
   pinMode(trig2, OUTPUT);
   pinMode(trig3, OUTPUT);
   pinMode(ena, OUTPUT);
+  pinMode(melodyPin, OUTPUT);
+  sing(1, true);
   Serial.begin(9600);
 }
-
 // todo:
 //  - just sweeping mode
 //  - just distance sensing/adjusting mode
@@ -30,33 +220,64 @@ void loop() {
   // v = v_max*sin(t)
   // positive velocity -> m1 HIGH m2 LOW
   // negative velocity -> m1 LOW m2 HIGH
-  int t = 0;
-  int ms_inc = 500;
-  int period = 6000;
-  double lastVelocity = 0;
+  boolean modeSweep = false;
   while (true) {
+    if (modeSweep) {
+      int t = 0;
+      int ms_inc = 500;
+      int period = 6000;
+      double lastVelocity = 0;
+      while (true) {
+        double v = 255 * sin(2 * PI * t * ms_inc / period);
+        if (lastVelocity > 0 && v < 0) {
+          // reverse direction to "positive"
+          digitalWrite(m1, HIGH);
+          digitalWrite(m2, LOW);
+        } else if (lastVelocity < 0 && v > 0) {
+          digitalWrite(m1, LOW);
+          digitalWrite(m2, HIGH);
+        }
+        Serial.println(v);
+        analogWrite(ena, abs(int(v)));
+        lastVelocity = v;
+        delay(ms_inc);
+        t++;
     
-    //    int d1 = getDistance(echo1, trig1);
-    //    int d2 = getDistance(echo2, trig2);
-    //    int d3 = getDistance(echo3, trig3);
-    double v = 255 * sin(2 * PI * t * ms_inc / period);
-    if (lastVelocity > 0 && v < 0) {
-      // reverse direction to "positive"
-      digitalWrite(m1, HIGH);
-      digitalWrite(m2, LOW);
-    } else if (lastVelocity < 0 && v > 0) {
-      digitalWrite(m1, LOW);
-      digitalWrite(m2, HIGH);
+      }
+    } else {
+      // d1 -> leftmost
+      // d3 -> rightmost
+      while (true) {
+        int avgd1 = averageDistances(echo1, trig1, 3);
+        int avgd2 = averageDistances(echo2, trig2, 3);
+        int avgd3 = averageDistances(echo3, trig3, 3);
+        if (avgd2 < 70) {
+          if (avgd1 > avgd2 && avgd1 > avgd3) {
+            // choose left
+            writeMotor(HIGH, LOW, 255);
+            sing(2, true);
+          }
+          if (avgd3 > avgd2 && avgd3 > avgd1) {
+            // choose right
+            writeMotor(LOW, HIGH, 255);
+            sing(2, true);
+          }
+          delay(1000);
+        } else {
+          writeMotor(LOW, LOW, 0);
+        }
+      }
+      
     }
-    Serial.println(v);
-    analogWrite(ena, abs(int(v)));
-    lastVelocity = v;
-    delay(ms_inc);
-    t++;
-
   }
+}
 
-
+int averageDistances(int echo, int trig, int s) {
+  int total = 0;
+  for (int i = 0; i < s; i++) {
+    total += getDistance(echo, trig);
+  }
+  return total/s;
 }
 
 int getDistance(int echoPin, int trigPin) {
@@ -74,4 +295,80 @@ int getDistance(int echoPin, int trigPin) {
   int distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
   // returns distance
   return distance;
+}
+
+void writeMotor(int m1val, int m2val, int motorSpeed) {
+  digitalWrite(m1, m1val);
+  digitalWrite(m2, m2val);
+  analogWrite(ena, motorSpeed);
+}
+
+
+int song = 0;
+
+void sing(int s, bool su) {
+  // iterate over the notes of the melody:
+  song = s;
+  if (song == 2) {
+    int size = sizeof(underworld_melody) / sizeof(int);
+    int sub = (su) ? 50 : 0;
+    for (int thisNote = 0; thisNote < size-sub; thisNote++) {
+
+      // to calculate the note duration, take one second
+      // divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration = 1000 / underworld_tempo[thisNote];
+
+      buzz(melodyPin, underworld_melody[thisNote], noteDuration);
+
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+
+      // stop the tone playing:
+      buzz(melodyPin, 0, noteDuration);
+
+    }
+
+  } else {
+
+    int size = sizeof(melody) / sizeof(int);
+    for (int thisNote = 0; thisNote < size/2; thisNote++) {
+
+      // to calculate the note duration, take one second
+      // divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration = 1000 / tempo[thisNote];
+
+      buzz(melodyPin, melody[thisNote], noteDuration);
+
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+
+      // stop the tone playing:
+      buzz(melodyPin, 0, noteDuration);
+
+    }
+  }
+}
+
+void buzz(int targetPin, long frequency, long length) {
+  digitalWrite(13, HIGH);
+  long delayValue = 1000000 / frequency / 2; // calculate the delay value between transitions
+  //// 1 second's worth of microseconds, divided by the frequency, then split in half since
+  //// there are two phases to each cycle
+  long numCycles = frequency * length / 1000; // calculate the number of cycles for proper timing
+  //// multiply frequency, which is really cycles per second, by the number of seconds to
+  //// get the total number of cycles to produce
+  for (long i = 0; i < numCycles; i++) { // for the calculated length of time...
+    digitalWrite(targetPin, HIGH); // write the buzzer pin high to push out the diaphram
+    delayMicroseconds(delayValue); // wait for the calculated delay value
+    digitalWrite(targetPin, LOW); // write the buzzer pin low to pull back the diaphram
+    delayMicroseconds(delayValue); // wait again or the calculated delay value
+  }
+  digitalWrite(13, LOW);
+
 }
